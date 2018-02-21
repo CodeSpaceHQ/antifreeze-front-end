06203eeecda4d77cbb205d407a2de8d916b27917
NilsG-S <nils.griswold.steiner@gmail.com>
2018-02-20 18:02:49 -0600

    Got react working

--- webpack/base.js
+++ webpack/base.js
@@ -1,8 +1,30 @@
 const path = require('path');
 
+const appPath = path.resolve(__dirname, '../src');
 const nodePath = path.resolve(__dirname, '../node_modules');
+const buildPath = path.resolve(__dirname, '../public');
 
 const base = {
+  // Allows for absolute paths from locations indicated in `resolve.modules`
+  resolve: {
+    modules: [
+      appPath,
+      nodePath,
+    ],
+  },
+
+  output: {
+    path: buildPath,
+    publicPath: '/',
+    filename: 'bundle.js',
+  },
+
+  entry: {
+    app: [
+      './src/index.jsx',
+    ],
+  },
+
   module: {
     /*
       Loaders for specific file endings. Each one can be composed
