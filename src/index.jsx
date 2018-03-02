10f5afc96e33c00ef128e44bfdfabfc3a97bf7fa
Robin Ronson <robin_ronson@yahoo.com>
2018-03-02 13:31:38 -0600

    added service workers and material ui v1

--- src/index.jsx
+++ src/index.jsx
@@ -1,5 +1,12 @@
 import React from 'react';
-import { render } from 'react-dom';
+import ReactDom from 'react-dom';
+import { cyan500, orange300, blueGrey400, limeA100, grey400, darkBlack, purpleA100, lightBlue50 } from 'material-ui/colors';
+import injectTapEventPlugin from 'react-tap-event-plugin';
+import getMuiTheme from 'material-ui/styles';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import { browserHistory, Router } from 'react-router';
+import routes from './routes.js';
+import notification from './notification.js';
 
 render(
   <h1>stuff</h1>,
