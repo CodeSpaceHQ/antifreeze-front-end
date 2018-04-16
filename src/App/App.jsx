ee603577f161db1da6e047df78ff0ad9cbcba724
Rachel <rachelcramblitt@gmail.com>
2018-04-16 17:35:27 -0500

    WIP: GraphPage not working

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -5,10 +5,11 @@ import { connect } from 'react-redux';
 import { history } from '../_helpers';
 import { alertActions } from '../_actions';
 import { PrivateRoute } from '../_components';
-import HomePage from '../HomePage/home.jsx'
+import HomePage from '../HomePage/home.jsx';
 import { LoginPage } from '../LoginPage';
 import { RegisterPage } from '../RegisterPage';
-import ButtonAppBar from '../Material/ButtonAppBar.jsx'
+import { GraphPage } from '../GraphPage/GraphPage.jsx';
+import ButtonAppBar from '../Material/ButtonAppBar.jsx';
 
 class App extends React.Component {
     constructor(props) {
@@ -29,9 +30,11 @@ class App extends React.Component {
                     <div>
                         <Router history={history}>
                             <div>
-                                <PrivateRoute exact path="/" component={HomePage} />
+                                <PrivateRoute exact path="/" component={HomePage} /> 
+                                {/* FIX ABOVE ME WITH component={HomePage} */}
                                 <Route path="/login" component={LoginPage} />
                                 <Route path="/register" component={RegisterPage} />
+                                <Route path="/graph" component={GraphPage} />
                                 {alert.message &&
                                 <div className={`alert ${alert.type}`}>{alert.message} </div>
                                 }
---
+++ src/GraphPage/GraphPage.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import ButtonAppBar from '../Material/ButtonAppBar.jsx';
+
+class GraphPage extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>TEXT HERE</h1>
+        <ButtonAppBar />
+      </div>
+    );
+  }
+}
+export default GraphPage;
