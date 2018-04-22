3764b7bcf98efc47cfbccc548a40e370d77927aa
l1mb0 <robin_ronson@yahoo.com>
2018-04-22 01:39:19 -0500

    Added SRC image

--- src/GraphPage/TempCard.jsx
+++ src/GraphPage/TempCard.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { withStyles } from 'material-ui/styles';
-import Card, { CardActions, CardContent } from 'material-ui/Card';
+import Card, { CardActions, CardContent, CardMedia } from 'material-ui/Card';
 import Button from 'material-ui/Button';
 import Typography from 'material-ui/Typography';
 import Switch from './Switch.jsx';
@@ -27,6 +27,10 @@ const styles = {
   pos: {
     marginBottom: 12,
   },
+  media: {
+    height: 0,
+    paddingTop: '35.00%', // 16:9
+  },
 };
 
 function SimpleCard(props) {
@@ -36,10 +40,16 @@ function SimpleCard(props) {
   return (
     <div>
       <Card className={classes.card}>
+      <CardMedia
+          className={classes.media}
+          image="notifications.png"
+          title="Contemplative Reptile"
+        />
+
         <CardContent style= {{paddingBottom: '0px'}}>
-          <Typography className={classes.title} color="textSecondary">
-            Temperature Alert
-          </Typography>
+        <Typography gutterBottom variant="headline" component="h2">
+        Temperature Alert
+        </Typography>
         </CardContent>
         <CardActions  style= {{padding: '0px 4px', paddingBottom: '30px'}}>
         {<Switch/>}
